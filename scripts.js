7c5d0f821b67a013b4f62bee9c29dd3b76677297
Andrew Williamson <awilliamson@Jerue.com>
2018-05-15 14:06:34 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function nav(page) {
+  switch(page){
+    case 'home':
+      return 
+    case 'news':
+      return 
+    case 'updates':
+      return
+    case 'faq':
+      return 
+    case 'contact':
+      return 
+  }
+}
